bfa647c0ae0627fd33ec1c4f6ed5cbcc76855ad1
Pete Pirasis <the.pete.the.pete@gmail.com>
2017-01-21 21:45:06 +0700

    fix test

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,6 @@
 const babelRegister = require('babel-register')
 require('core-js/shim')
 
-const babelConfig = require('../configuration/babel/babel.test.js')
+const babelConfig = require('../config/babel/babel.test.js')
 
 babelRegister(babelConfig)
