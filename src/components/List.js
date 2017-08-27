8a3c3015facdf017e5884d30c6e4734b809a5174
Pirasis <1pete@users.noreply.github.com>
2017-08-27 17:36:01 +0700

    update project dependencies

--- src/components/List.js
+++ src/components/List.js
@@ -1,88 +1,36 @@
-import React, { Component } from 'react'
-import { TransitionMotion, spring, presets } from 'react-motion'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import Item from '../containers/InteractiveItem'
 
 import './List.css'
 
-class List extends Component {
-  static propTypes = {
-    flag: PropTypes.bool,
-    items: PropTypes.array.isRequired,
-  }
+const List = ({ items, flag }) => {
+  const filteredItems = flag == null ? items : items.filter(item => !!item.completed === flag)
 
-  static defaultProps = {
-    flag: null,
-  }
-
-  getDefaultStyles() {
-    return this.props.items.map(item => ({
-      data: item,
-      key: item.id,
-      style: { height: 0, opacity: 1 },
-    }))
-  }
-
-  getStyles() {
-    let { items, flag } = this.props
-
-    if (flag != null) {
-      items = items.filter(i => !!i.completed === flag)
-    }
-
-    return items
-      .map(item => ({
-        data: item,
-        key: item.id,
-        style: {
-          height: spring(42, presets.gentle),
-          opacity: spring(1, presets.gentles),
-        },
-      }))
-  }
-
-  willEnter() {
-    return {
-      height: 0,
-      opacity: 0,
-    }
-  }
-
-  willLeave() {
-    return {
-      height: spring(0),
-      opacity: spring(0),
-    }
-  }
-
-  render() {
-    let styles = this.getStyles()
-
-    return (
-      <div>
-        <div className="pt-non-ideal-state" style={{ opacity: +!styles.length }}>
-          <div className="pt-non-ideal-state-visual pt-non-ideal-state-icon">
-            <span className="pt-icon pt-icon-inbox" />
-          </div>
-          <h4 className="pt-non-ideal-state-title">List is empty</h4>
-          <div className="pt-non-ideal-state-description">Try create a new to-do.</div>
+  return (
+    <div>
+      <div className="pt-non-ideal-state" style={{ opacity: +!filteredItems.length }}>
+        <div className="pt-non-ideal-state-visual pt-non-ideal-state-icon">
+          <span className="pt-icon pt-icon-inbox" />
         </div>
-        <TransitionMotion
-          defaultStyles={this.getDefaultStyles()}
-          styles={styles}
-          willLeave={this.willLeave}
-          willEnter={this.willEnter}
-        >
-          {s => (
-            <ul className="list-container">
-              {s.map(({ key, style, data }) => <Item key={key} style={style} {...data} />)}
-            </ul>
-          )}
-        </TransitionMotion>
+        <h4 className="pt-non-ideal-state-title">List is empty</h4>
+        <div className="pt-non-ideal-state-description">Try create a new to-do.</div>
       </div>
-    )
-  }
+      <ul className="list-container">
+        {filteredItems.map(data => <Item key={data.id} {...data} />)}
+      </ul>
+    </div>
+  )
+}
+
+List.propTypes = {
+  flag: PropTypes.bool,
+  items: PropTypes.array.isRequired,
+}
+
+List.defaultProps = {
+  flag: null,
 }
 
 export default List
