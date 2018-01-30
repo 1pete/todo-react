a6e964a39cd9c65bdd1fda4153906e4572fac126
Pirasis <1pete@users.noreply.github.com>
2018-01-31 01:29:40 +0700

    fail ci

--- test/misc/utils.spec.js
+++ test/misc/utils.spec.js
@@ -4,6 +4,6 @@ import * as utils from '../../src/utils'
 
 describe('utils', () => {
   it('should get uuid correctly', () => {
-    expect(utils.uuid()).to.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/)
+    expect(utils.uuid()).to.match(/^a[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/)
   })
 })
